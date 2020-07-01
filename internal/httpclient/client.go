9480f5484e855df192185565268eea69f1d452f0
Alex Kataev <dlyavsehpisem@gmail.com>
2020-07-02 00:25:01 +0500

    fix response error

--- internal/httpclient/client.go
+++ internal/httpclient/client.go
@@ -119,9 +119,9 @@ func (c *client) Send(ctx context.Context, data io.Reader) error {
 		respErr := &responseError{}
 
 		if err := json.Unmarshal(body, respErr); err != nil {
-			sendErr.HTTPError = string(body)
+			sendErr.HTTPError = strings.ReplaceAll(string(body), "\n", " ")
 		} else {
-			sendErr.ResponseError = strings.ReplaceAll(respErr.Error, "\n", " ")
+			sendErr.ResponseError = respErr.Error
 		}
 
 		return sendErr
